20d54e6e33197ee1c9169e0d37019ff3aaeee086
Justin Byrd <justinbyrd7@gmail.com>
2023-12-14 09:46:50 -0600

    wip

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,14 +1,26 @@
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = require("../../config");
 // AUTHENTICATION
 const restricted = (req, res, next) => {
-  next()
-}
+  //extract the token from the request
+  const token = req.headers.authorization;
+  token
+    ? jwt.verify(token, JWT_SECRET, (err, decoded) => {
+        err
+          ? next({ status: 401, message: "token invalid" })
+          : (req.decodedJwt = decoded),
+          console.log(req.decodedJwt),
+          next();
+      })
+    : next({ status: 401, message: "token required" });
+};
 
 // AUTHORIZATION
 const checkRole = (req, res, next) => {
-  next()
-}
+  next();
+};
 
 module.exports = {
   restricted,
   checkRole,
-}
+};
